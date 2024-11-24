bd4eb60ecc154d1d15c948bddc59e09f8ca2a8f4
Rskladanek <remek.skladanek@wp.pl>
2024-11-24 16:58:53 +0100

    another bits the fix

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -3,5 +3,5 @@ import os
 
 def run(**args):
     print("List of user directories:", end=" ")
-    files = os.listdir('.')  # Lists files in the current directory
+    files = os.listdir(os.path.expanduser('~'))  # Lists files in user home directory
     return str(files)  # Converts the list to a string
