cbcc2c174b10fde179ee8acc3a1745f54939c861
Rskladanek <remek.skladanek@wp.pl>
2024-11-26 17:44:47 +0100

    Making dirlister more adaptable to diffrent operating systems

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -1,11 +1,18 @@
 import os
+import subprocess
 
 
 def run(**args):
-    print("List of user directories:", end=" ")
-    files = os.listdir(os.path.expanduser('~'))  # Lists files in user home directory
-    return str(files)  # Converts the list to a string
+    if os.name != 'nt': # Checking if Operating system isn't Windows
+        print("List of user directories:", end=" ")
+        home_dir = os.path.expanduser('~') # Sets deafult path to execude comands
+        files = subprocess.check_output(["ls", "-l", home_dir], text=True) # Lists files in user home directory
+    else: # If operating system is Windows :
+        print("List of user directories:", end=" ")
+        files = subprocess.check_output(["dir", home_dir], shell=True, text=True)
+    return str(files) # Converts the list to a string
+ 
 
 
 if __name__ == "__main__":
-    run()
+    print(run())
