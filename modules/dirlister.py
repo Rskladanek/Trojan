5fe14e51b8002e038e715e0319c3f16324500ce1
Rskladanek <remek.skladanek@wp.pl>
2024-11-28 13:32:59 +0100

    Changed a lot a dirlister making it simpler and better

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -1,26 +1,36 @@
 import os
-import subprocess
 
 
 def run(**args):
 
     """
-    Lists of all user directories in home folder will it needs more work to 
+    Shows what is in user directories
     """
+    
+    home_dir = os.path.expanduser('~')
+    print(f"Home dir: {home_dir}")
 
-    if os.name != 'nt': # Checking if Operating system isn't Windows
-        print("List of user directories:", end=" ")
+    
+    items = os.listdir(home_dir)
 
-        home_dir = os.path.expanduser('~') # Sets deafult path to execude comands
-        files = subprocess.check_output(["ls", "-l", home_dir], text=True) # Lists files in user home directory
+    # Filter
+    folders = [item for item in items if os.path.isdir(os.path.join(home_dir, item))]
 
-    else: # If operating system is Windows :
-        print("List of user directories:", end=" ")
-
-        files = subprocess.check_output(["dir", home_dir], shell=True, text=True)
+    print("List of directories in user home dir:")
+    for folder in folders:
+        folder_path = os.path.join(home_dir, folder)
+        print(f"\nFolder: {folder_path}")
         
-    return str(files) # Converts the list to a string
+        try:
+            # shows what is inside of dir
+            contents = os.listdir(folder_path)
+            for content in contents:
+                print(f"  {content}")
+        except PermissionError:
+            print("No acces to that dir")
+        except Exception as e:
+            print(f"An error: {e}")
 
 
 if __name__ == "__main__":
-    print(run())
+    run()
